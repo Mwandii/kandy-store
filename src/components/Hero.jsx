935ea8e44f0d178dcca563aa049a5f80390ba1f7
Athanas Muinde <athanasmwandi5@gmail.com>
2026-02-01 23:55:20 +0300

    add more styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaShoppingBag, FaStore, FaCheckCircle, FaStar } from "react-icons/fa";
 
 function HeroSection({ setCurrentPage }) {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav>
+    <nav className="">
       <div className="flex justify-between px-5 py-2 bg-linear-to-r from-amber-600 via-amber-700 to-orange-600 text-white text-sm">
         <p>For all your baby essentials. Fast and reliable delivery guaranteed</p>
         <div className="flex gap-4 hover:cursor-pointer">
@@ -10,7 +10,7 @@ function Navbar() {
       </div>
     <div className="flex px-6 py-4 justify-between items-center bg-white/95 backdrop-blur-md">
       <h1 className="font-bold text-3xl bg-linear-to-r from-amber-600 via-orange-600 to-amber-700 bg-clip-text text-transparent">Kandy Baby Store</h1>
-      <input className="bg-gray-100 px-4 py-2 font-light rounded-full" placeholder="Search....."/>
+      <input type="search" className="bg-gray-100 px-6 py-4 font-light rounded-full w-154 text-sm" placeholder="Search for baby products, brands, shops......"/>
       <div className="flex gap-3">
         <button className="text-l font-semibold">Cart</button>
         <button className="text-l font-semibold">Login</button>
@@ -18,12 +18,12 @@ function Navbar() {
     </div>
     <hr className="w-49/50 mx-auto border-gray-200"/>
     <div>
-      <ul className="flex items-center gap-5 mx-5 py-5 text-gray-700 text-sm">
+      <ul className="flex items-center gap-5 mx-5 py-2 text-gray-700 text-sm font-bold">
         <li className="hover:cursor-pointer"><a>All Categories</a></li>
         <li className="hover:cursor-pointer"><a>New Arrivals</a></li>
         <li className="hover:cursor-pointer"><a>Best Deals</a></li>
         <li className="hover:cursor-pointer"><a>Top Vendors</a></li>
-        <li className="hover:cursor-pointer bg-linear-to-r from-amber-600 to-orange-600 px-6 py-2 text-white rounded-full"><a>Start Selling</a></li>
+        <li className="hover:cursor-pointer bg-linear-to-r from-amber-600 to-orange-600 px-6 py-2 text-white rounded-full shadow-md"><a>Start Selling</a></li>
       </ul>
     </div>
     </nav>
